38f775563524f439171f02acab0c2687bd79745a
Anas Amer <anasamer97@gmail.com>
2025-06-26 13:29:21 +0300

    feat: add obstacle check before rover movement

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ let rover = {
     stopped: false
 }
 
-        // NORTH
-
-//West            // EAST
-        // SOUTH
 let directions = {
     north: {x: 0, y:1},
     east: {x:1, y:0},
@@ -33,42 +29,30 @@ function getCommand(commandString) {
     for(let i = 0; i < commandString.length; i++) {
         if(commandString[i] === 'F' || commandString[i] === 'B') {
             const direction = directions[rover.direction];
-            const multipler = commandString[i] === 'F' ? 1 : -1;
-            const nextX = rover.x + direction.x * multipler;
-            const nextY = rover.y + direction.y * multipler;
+            const multiplier = commandString[i] === 'F' ? 1 : -1;
+            const nextX = rover.x + direction.x * multiplier;
+            const nextY = rover.y + direction.y * multiplier;
 
-            if(isObstacle(nextX, nextY)) {
-                console.log("isObstacle trigger");
-                rover.stopped = true;
-                return `Current Position (${rover.x}, ${rover.y}) ${rover.direction.toUpperCase()} STOPPED. Encounterd an obstacle at (${nextX}, ${nextY}). `
+            if (isObstacle(nextX, nextY)) {
+                return `Current Position (${rover.x}, ${rover.y}) ${rover.direction.toUpperCase()} STOPPED. Encountered an obstacle at (${nextX}, ${nextY}).`;
             }
-           
+
             rover.x = nextX;
             rover.y = nextY;
         }
-
-        else if(commandString[i] === 'B') {
-            rover.x -= directions[rover.direction].x
-            rover.y -= directions[rover.direction].y
-        }
-
         else if(commandString[i] === 'R') {
-            let currentIndex = coordinates.indexOf(rover.direction)
+            let currentIndex = coordinates.indexOf(rover.direction);
             let nextIndex =  (currentIndex + 1) % coordinates.length;
-            rover.direction = coordinates[nextIndex]
-            
+            rover.direction = coordinates[nextIndex];
         }
-
         else if (commandString[i] === 'L') {
-            let currentIndex = coordinates.indexOf(rover.direction)
-            let nextIndex =  (currentIndex - 1) % coordinates.length;
-            if (nextIndex === -1) 
-                nextIndex = 3
-            rover.direction = coordinates[nextIndex] 
+            let currentIndex = coordinates.indexOf(rover.direction);
+            let nextIndex =  (currentIndex - 1 + coordinates.length) % coordinates.length;
+            rover.direction = coordinates[nextIndex];
         }
     }
 
-    return `x: ${rover.x}, y: ${rover.y}, ${rover.direction}`
+    return `Current Position (${rover.x}, ${rover.y}) ${rover.direction.toUpperCase()}`;
 }
 
 
